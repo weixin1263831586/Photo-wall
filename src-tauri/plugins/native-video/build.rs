f35dfcd9761dbc1e722e749e0dad52b7b1e8f369
Huang Chaoqun <36125448+weixin1263831586@users.noreply.github.com>
2026-08-18 16:04:15 +0800

    native-video: generate permissions for all commands

--- src-tauri/plugins/native-video/build.rs
+++ src-tauri/plugins/native-video/build.rs
@@ -1,4 +1,10 @@
-const COMMANDS: &[&str] = &["capabilities", "transcode"];
+const COMMANDS: &[&str] = &[
+    "capabilities",
+    "transcode",
+    "open_file",
+    "extract_poster",
+    "transcode_frames",
+];
 
 fn main() {
     tauri_plugin::Builder::new(COMMANDS)
